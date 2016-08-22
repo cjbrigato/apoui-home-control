a7053c07d71759bb1a74713d95de4817ef59dbb9
Colin J. Brigato <colin@brigato.fr>
2016-08-22 09:32:49 +0200

    Security Breach v1

--- rfid/security-process.sh
+++ rfid/security-process.sh
@@ -2,6 +2,7 @@
 CDIR=`dirname $0`
 cd $CDIR
 
+HOST="control.maison.apoui.net"
 LOGFILE="../logs/doorlock.log"
 SECURITY=""
 
@@ -14,6 +15,10 @@ getsecstatus
 if [ "$SECURITY" == "OFF" ];then
   echo "-SECURITY- Security is OFF : Nothing to do" >> $LOGFILE
 elif [ "$SECURITY" == "ON" ]; then
+  #curl -m 1.5 "http://$HOST/setrelay/5/on"  # TV ON !!
+  curl -m 1.5 "http://$HOST/setrelay/1/off"
+  curl -m 1.5 "http://$HOST/setrelay/2/off"
+  curl -m 1.5 "http://hd1.freebox.fr/pub/remote_control?code=77460495&key=power" # TV ON !!
   echo "-SECURITY- SECURITY IS ON : PROCEDURE ENGAGED !" >> $LOGFILE
   echo "-SECURITY- Waiting 15 SEC FOR DEAC..." >> $LOGFILE
   sleep 15
@@ -22,6 +27,8 @@ elif [ "$SECURITY" == "ON" ]; then
     echo "-SECURITY- Security DISENGAGED : Return to Normal" >> $LOGFILE
   elif [ "$SECURITY" == "ON" ]; then
     echo "-SECURITY- SECURITY BREACH CONFIRMED. PROCEED NOW !" >> $LOGFILE
+    ssh root@127.0.0.1 "source /etc/profile && source /root/.bashrc && bash -c '/usr/local/rvm/rubies/ruby-2.2.4/bin/ruby /usr/local/rvm/gems/ruby-2.2.4/bin/airstream //chroot/nginx-static/www/control.maison.apoui.net/rfid/secbreachfull.mp4'"
+
   fi
 fi
 
